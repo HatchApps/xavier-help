57ade4decb36e28a8ca9b9c4a68f13f42f06fb18
Rich Nicolson <richard.nicolson@gmail.com>
2018-08-21 09:47:52 +0100

    Favicon and related metadata

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,19 @@
 module.exports = {
     title: 'Xavier Analytics - Help',
-    description: '',
+    description: 'Help documentation and support for Xavier Analytics',
     ga: 'UA-76675321-9',
+    head: [
+        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+        ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+        ['meta', { name: 'theme-color', content: '#ffffff' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+        ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#393142' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/mstile-150x150.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#9f00a7' }]
+    ],
     themeConfig: {
         sidebar: [
             '/',
